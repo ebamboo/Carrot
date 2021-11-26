db751bb97b5196928b99db620e38eb6cd8140ee5
ebamboo <1453810050@qq.com>
2021-11-26 22:14:08 +0800

    初始化

---
+++ Carrot/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Carrot
+//
+//  Created by ebamboo on 2021/11/26.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
